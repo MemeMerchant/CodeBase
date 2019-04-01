b6bc4c93ac1ae8309f7160d6d3cff21e37f92420
ericbrown <ericbrown1.2018@u.northwestern.edu>
2019-04-01 09:04:34 -0700

    Cabo Commit #4: Compiled all contract code; needs to be edited for functionality now

---
+++ contracts/MemeCore.sol
@@ -0,0 +1,55 @@
+  pragma solidity >= 0.4.21 <0.6.0;
+
+import "./MemeMinting.sol";
+
+
+/// @title MemeMerchant: Trade, collect, and create memes on the blockchain
+/// @dev The core contract which contains all other contracts for MemeMerchant.
+
+contract MemeCore is MemeMinting{
+
+  address public newContractAddress;
+
+  constructor() public {
+    paused = true;
+
+    ceoAddress = msg.sender;
+
+    cooAddress = msg.sender;
+
+  }
+
+  function setNewAddress(address _nextAddress) public onlyCEO whenPaused{
+    newContractAddress = _nextAddress;
+    emit ContractUpgrade(newContractAddress);
+  }
+
+  function() external payable{
+    require(
+      msg.sender == address(clockAuction)
+      );
+  }
+
+  function getMeme(uint256 _memeId)
+    public
+    view
+    returns(
+      address owner,
+      address creator,
+      uint16 generation,
+      uint64 birthTime
+      ){
+        Meme storage _meme = memes[_memeId];
+        owner = memeIdToOwner[_memeId];
+        creator = _meme.creator;
+        generation = _meme.generation;
+        birthTime = _meme.birthTime;
+      }
+
+   function unpause() public onlyCEO{
+     require(address(clockAuction) != address(0));
+     require(newContractAddress != address(0));
+
+     super.unpause();
+   }
+}
---
+++ contracts/MemeMinting.sol
@@ -0,0 +1,43 @@
+pragma solidity >=0.4.21 <0.6.0;
+
+import "./MemeAuction.sol";
+
+/// @dev Functions releated to creating new memes
+contract MemeMinting is MemeAuction{
+
+  uint128 legacyMemeStartingPrice = 10 finney;
+  uint64 legacyMemeAuctionTime = 1 days;
+  uint256 legacyCreatedCount;
+
+  function createLegacyAuction(uint16 _generation) public onlyCOO{
+    uint256 memeId = createMeme(address(this),address(0),_generation);
+    _approve(address(clockAuction), memeId);
+
+    clockAuction.createAuction(
+      memeId,
+      _computeNextLegacyPrice(),
+      0,
+      legacyMemeAuctionTime,
+      address(uint(address(this)))
+      );
+
+    legacyCreatedCount++;
+  }
+
+
+  function _computeNextLegacyPrice() internal view returns (uint256) {
+        uint256 avePrice = clockAuction.averageLegacyMemePrice();
+
+        // sanity check to ensure we don't overflow arithmetic (this big number is 2^128-1).
+        require(avePrice < 340282366920938463463374607431768211455);
+
+        uint256 nextPrice = avePrice + (avePrice / 2);
+
+        // We never auction for less than starting price
+        if (nextPrice < legacyMemeStartingPrice) {
+            nextPrice = legacyMemeStartingPrice;
+        }
+
+        return nextPrice;
+    }
+}
