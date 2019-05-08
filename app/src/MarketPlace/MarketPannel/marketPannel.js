bd701c94f5782f35d6e06f6683c582b5d4a97e4b
ericbrown <ericbrown1.2018@u.northwestern.edu>
2019-05-07 22:58:34 -0400

    meme popups working

--- app/src/MarketPlace/MarketPannel/marketPannel.js
+++ app/src/MarketPlace/MarketPannel/marketPannel.js
@@ -1,7 +1,9 @@
 import React, {useState, useEffect} from "react";
 import "./marketPannel.css";
 import getMemeContract from "../../MemeContract/memeContract.js";
-import getClockAuction from "../../ClockAuction/clockAuction.js"
+import getClockAuction from "../../ClockAuction/clockAuction.js";
+import Banner from "../../Banner/banner.js";
+import Web3 from "web3";
 
  function MarketPannel(props){
   // use searchType prop to filter meme's
@@ -12,6 +14,8 @@ import getClockAuction from "../../ClockAuction/clockAuction.js"
   const [memeArray, setMemeArray] = useState();
   const [memeContract,setMemeContract]= useState(0);
   const [auctionContract, setAuctionContract] = useState(0);
+  const [memePopUp, setMemePopUp] = useState(null);
+  const [web3, setWeb3] = useState(new Web3("ws://localhost:8545"));
 
 
   if(memeContract == 0){
@@ -55,47 +59,107 @@ import getClockAuction from "../../ClockAuction/clockAuction.js"
   }
 
 
-    async function renderArray(){
-      if(props.searchType === "All"){
-           console.log(arrayLength);
-           var arr = []
-           for(var i = 0; i < arrayLength; i++){
-            await arr.push(i);
-            console.log(i);
-           }
-           console.log(arr);
-           await setMemeArray(arr);
-           console.log(memeArray);
-          }
+  async function renderArray(){
+    if(props.searchType === "All"){
+         console.log(arrayLength);
+         var arr = []
+         for(var i = 0; i < arrayLength; i++){
+          await arr.push(i);
+          console.log(i);
+         }
+         console.log(arr);
+         await setMemeArray(arr);
+         console.log(memeArray);
+        }
 
-      if(props.searchType === "ForSale"){
-          console.log("FORSALE")
-          var arr =[]
-          console.log(auctionContract);
-          for(var i=0; i<arrayLength; i++){
-           let res = await auctionContract.methods.getOnAuction(i).call();
-           if(res){arr.push(i)};
-          }
-          setMemeArray(arr);
-          console.log(arr);
-      }
+    if(props.searchType === "ForSale"){
+        console.log("FORSALE")
+        var arr =[]
+        console.log(auctionContract);
+        for(var i=0; i<arrayLength; i++){
+         let res = await auctionContract.methods.getOnAuction(i).call();
+         if(res){arr.push(i)};
+        }
+        setMemeArray(arr);
+        console.log(arr);
+    }
 
-      if(props.searchType === "Legacy"){
-          var arr = [];
-          for(var i =0; i<arrayLength; i++){
-            let res =  await memeContract.methods.isLegacyMeme(i).call();
-            if(res){arr.push(i);}
-          }
-          setMemeArray(arr);
-          console.log(arr);
-          // know length, loop through array of meme's within lenght
-          // in loop check if generation == o and add to new array
+    if(props.searchType === "Legacy"){
+        var arr = [];
+        for(var i =0; i<arrayLength; i++){
+          let res =  await memeContract.methods.isLegacyMeme(i).call();
+          if(res){arr.push(i);}
+        }
+        setMemeArray(arr);
+        console.log(arr);
+        // know length, loop through array of meme's within lenght
+        // in loop check if generation == o and add to new array
+    }
+  }
+
+  console.log(memeArray)
+  console.log(memePopUp);
+
+  function handleLoadGrids() {
+    let total = arrayLength;
+    let numRows = total / 4;
+    numRows = Math.ceil(numRows);
+    let arr =[];
+    for(let i = 0; i < numRows; i++){
+        arr.push(<MarketPannelRow array={memeArray} Toggle={togglePopUp}/>)
+    }
+    return(arr);
+  }
+
+  function togglePopUp(x){
+    if(memePopUp !== x){
+      setMemePopUp(x);
+    }else{
+      if(memePopUp != null){
+        return(renderPopUp());
       }
     }
 
-    console.log(memeArray)
+  }
 
+  function renderPopUp(){
+    let memeId = memePopUp;
+    console.log(memeId + "This is in pop up");
+    let memeName;
+    if(memeId == 0){
+      console.log("aliens")
+      memeName = "Aliens";
+    }else if(memeId == 1){
+      console.log("aruthr")
+      memeName = "Arthur's Fist";
+    }
+    let owner;
+    let price;
+    if(memeId !== null){
+      return(
+        <div className='popup'>
+          <div className='popup_inner'>
 
+            <Banner
+              className="popUP"
+              title={memeName}
+              subTitle="The MemeCosts X ether"
+              buttonFunction={() => togglePopUp(null)}
+              buttonLabel={"Buy " + memeName}
+              rightHalf={
+                <div className="gridMeme">
+                  <img src={require('../../memes/meme-' + memeId + '.jpg')}/>
+                </div>
+              }
+            />
+          </div>
+        </div>
+      )
+    }else{
+      return(null)
+    }
+
+  }
 
   return(
     <div>
@@ -114,9 +178,13 @@ import getClockAuction from "../../ClockAuction/clockAuction.js"
             </div>
             :
             <div className="LoadingMemeGrids">
-              <MarketPannelRow array={memeArray}/>
+              {handleLoadGrids()}
             </div>
-           }
+          }
+          {
+           togglePopUp(memePopUp)
+
+          }
          </div>
        }
    </div>
@@ -124,27 +192,36 @@ import getClockAuction from "../../ClockAuction/clockAuction.js"
 }
 
 
-
-
+// make a for loop that Generates Market Pannel Rows based on number of meme's
+// divide by 4 and then loop up to the cieling
 
 
 
 function MarketPannelRow(props){
   let array = props.array;
+  let toggleButton = props.Toggle;
+
 
   let retArray = [];
-
-  for(var i =0; i < array.length; i++){
+  for(var i = 0; i < array.length; i++){
+    let id = array[i];
     retArray.push(
       <div className="marketMeme">
-        <button>
-          <img src={require('../../memes/meme-' + i + '.jpg')}/>
-        </button>
+      <button onClick={() => toggleButton(id)}>
+        <img src={require('../../memes/meme-' + id + '.jpg')}/>
+      </button>
       </div>
     )
   }
   return(retArray)
-
 }
+
+
+
+
+
+
+
+
 
 export default MarketPannel;
